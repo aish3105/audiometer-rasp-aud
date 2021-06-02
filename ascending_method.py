9cd4faf7b2844715fa6400f0689109ccc704e41d
Aishwarya M <70625095+aish3105@users.noreply.github.com>
2021-06-02 23:42:23 +0530

    Detailed instructions
    Makes the device more user friendly. Test can be carried out without technical assistance.

--- ascending_method.py
+++ ascending_method.py
@@ -29,6 +29,8 @@ class AscendingMethod:
     print("\n\n\n\n*****************************************************************WELCOME!!**********************************************************************************)
     time.sleep(3)
     
+    print("\n\nREAD THE INSTRUCTIONS CAREFULLY BEFORE THE TEST BEGINS\n\n1) To begin with decrease or increase the tone intensity with '1' or '2' NUM KEYS according to the hearing comfort\n2) Once the desired tone level is set press on the space bar to proceed for the test\n3) Click the mouse button once to begin the test\n4) Kindly press and hold the left mouse button whenever the played tone is heard to you each time")
+    
 
     def __init__(self):
         self.ctrl = controller.Controller()
